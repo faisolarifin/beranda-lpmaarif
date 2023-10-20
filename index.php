ecbe8a7555950eb210fd8d886cdc3d3fe10d1f9c
ach.arifin <ach.arifin@jatis.com>
2023-10-20 10:20:12 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="encoding" content="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta id="meta-application-name" name="application-name" content="Lembaga Pendidikan Ma'arif NU PBNU | Beranda" />
+    <meta id="meta-description" name="description" content="Lembaga Pendidikan Ma'arif NU PBNU Beranda" />
+    <meta id="meta-keywords" name="keywords" content="lp maarif, maarifnu, nu maarif nu, lembaga pendidikan, pendidikan maarifnu, nahdlatul ulama, nu, sekolah, madrasah, sipinter, sipinter maarifnu" />
+        
+    <meta name='google' content='notranslate' />
+    <meta name='theme' content='Faisol Themes' />
+    <meta name='designer' content='Ach Faisol S Arifin' />
+    <meta name='theme:designer' content='Ach Faisol S Arifin' />
+    <meta name='theme:version' content='1.0' />
+    <meta property="og:title" content="Lembaga Pendidikan Ma'arif NU PBNU Beranda">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Lembaga Pendidikan Ma'arif NU PBNU Beranda"/>
+    <meta property="fb:app_id" content="147912828718">
+
+
+    <link rel="preload" href="./assets/img/LOGO-MAARIF-WEB.jpg" as="image">
+    <link rel="preload" href="./assets/img/Sipinter-LPMaarifNU.jpg" as="image">
+
+    <link rel="icon" href="./assets/img/LOGO-MAARIF-WEB.jpg" type="image/png">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@300;400;500&display=swap" rel="stylesheet">
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+    <title>Lembaga Pendidikan Ma'arif NU PBNU | Beranda</title>
+  </head>
+  <body>
+
+    <div class="container">
+        <div class="row" style="height:95vh;">
+          <div class="col d-flex flex-column justify-content-center align-items-center">
+            <h3 class="animate__animated animate__fadeInDown text-center mb-4">Aplikasi <br> Lembaga Pendidikan Ma'arif <br> Nahdlatul Ulama PBNU</h3>
+            <div class="card-grup d-flex">
+              <div class="animate__animated animate__flipInX card mx-3 shadow-sm" style="width: 12rem;">
+                <div class="card-body">
+                  <img src="./assets/img/LOGO-MAARIF-WEB.jpg" class="card-img-top mb-3" alt="...">
+                  <h5 class="card-title">Beranda LP Ma'arif <br> NU PBNU</h5>
+                  <a href="https://maarif.nu.or.id/" class="btn btn-green w-100">Kunjungi <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"/>
+                  </svg></a>
+                </div>
+              </div>              
+              <div class="animate__animated animate__flipInX card mx-3 shadow-sm" style="width: 12rem;">
+                <div class="card-body">
+                  <img src="./assets/img/Sipinter-LPMaarifNU.jpg" class="card-img-top mb-3" alt="...">
+                  <h5 class="card-title">Sipinter LP Ma'arif <br> NU</h5>
+                  <a href="https://sipinter.maarifnu.or.id/" class="btn btn-green w-100">Kunjungi <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"/>
+                  </svg></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col py-2">
+            <p class="mb-0">Copyright &copy; <?= date('Y') ?> Lembaga Pendidikan Ma'arif NU PBNU</p>
+          </div>
+        </div>
+        
+      </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
